d64a97a5e75810d6ec56903e4f02b74d441abb26
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-12-03 09:16:25 +0000

    2023 day 3 cleanup

--- 2023/3/part2.rb
+++ 2023/3/part2.rb
@@ -7,19 +7,17 @@ width = LINES[1].size - 2
 height = LINES.size - 1
 LINES[0] = '.' * (width + 2)
 LINES << '.' * (width + 2)
-NEIGHBOURS = ([1, 0, -1].repeated_combination(2).to_a - [0, 0]).
-  flat_map { |d| d.permutation(2).to_a.uniq }
+NEIGHBOURS = (-1..1).to_a.product((-1..1).to_a) - [[0, 0]]
 
 def digit?(c) = /\d/.match?(c)
 
 puts (1..height).reduce(0) { |sum, y|
-  line = LINES[y]
-  scanner = StringScanner.new(line)
+  scanner = StringScanner.new(LINES[y])
   until scanner.eos?
     scanner.scan_until(/\*/)
     break unless scanner.matched?
     x = scanner.pos - 1
-    ranges = NEIGHBOURS.map do |dx, dy|
+    ranges = NEIGHBOURS.filter_map do |dx, dy|
       line = LINES[y + dy]
       x_beg = x + dx
       next nil unless digit?(line[x_beg])
@@ -28,9 +26,7 @@ puts (1..height).reduce(0) { |sum, y|
       x_end += 1 while digit?(line[x_end])
       [y + dy, x_beg, x_end]
     end
-    nums = ranges.reject(&:nil?).uniq.map do |(y, b, e)|
-      LINES[y][b...e].to_i
-    end
+    nums = ranges.uniq.map { |(y, b, e)| LINES[y][b...e].to_i }
     next unless nums.size == 2
     sum += nums.reduce(:*)
   end
