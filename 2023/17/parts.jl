abca5f7c6a4b5a2bb42effbc5701635207cd24b7
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-12-18 04:40:16 +0000

    2023 day 17: optimize

--- 2023/17/parts.jl
+++ 2023/17/parts.jl
@@ -1,84 +1,79 @@
 #!/usr/bin/env julia
 using DataStructures
 
-const DIRS = (CartesianIndex(0, -1), CartesianIndex(1, 0),
-  CartesianIndex(0, 1), CartesianIndex(-1, 0))
-const UP = 1; const RIGHT = 2; const DOWN = 3; const LEFT = 4
-const OPPOSITE = (DOWN, LEFT, UP, RIGHT)
 const MAX_COST = typemax(Int)
 
-function dijkstra(data::Matrix{Int}, min_dist::Int, max_dist::Int)
-  dist = fill(MAX_COST, length(DIRS), max_dist, size(data)...)
-  queue = PriorityQueue{CartesianIndex{4}, Int}()
-  for p in CartesianIndices(data), dir in eachindex(DIRS), seq in 1:max_dist
-    queue[CartesianIndex(dir, seq, p)] = MAX_COST
+@views function dijkstra(data::Matrix{Int}, range::UnitRange{Int})
+  dist = fill(MAX_COST, 4, size(data)...)
+  prev = fill(CartesianIndex(-1, -1, -1), size(dist))
+  queue = PriorityQueue{CartesianIndex{3},Int}()
+  for i in CartesianIndices(axes(dist))
+    queue[i] = MAX_COST
   end
-  for (source, source_dist) in (
-    (CartesianIndex(DOWN, 1, 1, 2), data[1, 2]),
-    (CartesianIndex(RIGHT, 1, 2, 1), data[2, 1]))
-    dist[source] = queue[source] = source_dist
+  for source in (CartesianIndex(2, 1, 1), CartesianIndex(3, 1, 1))
+    dist[source] = queue[source] = 0
   end
+  Σ = (cumsum(data, dims=1), cumsum(transpose(data), dims=1))
   while !isempty(queue)
     u = dequeue!(queue)
+    xy_u = CartesianIndex(u[2], u[3])
+    xy_u == size(data) && return
     dist_u = dist[u]
     dist_u == MAX_COST && continue
-    for dir in eachindex(DIRS)
-      dir == OPPOSITE[u[1]] && continue
-      dir == u[1] && u[2] == max_dist && continue
-      dir != u[1] && u[2] < min_dist && continue
-      xy = CartesianIndex(u[3], u[4]) + DIRS[dir]
-      !checkbounds(Bool, data, xy) && continue
-      v = CartesianIndex(dir, dir == u[1] ? u[2] + 1 : 1, xy)
-      dist_v = get(queue, v, -1)
-      dist_v == -1 && continue
-      alt = dist_u + data[xy]
-      if alt < dist_v
-        queue[v] = alt
-        dist[v] = alt
-      end
-    end
-  end
-  dist
-end
-
-const DIR_STR = ['^', '>', 'v', '<']
-function print_path(data::Matrix{Int}, dist::Array{Int, 4}, min_dist::Int, max_dist::Int)
-  grid = data .|> (x -> '0' + x)
-  print_grid(grid) = foreach(col -> println(prod(col)), eachcol(grid))
-
-  cur = CartesianIndex(argmin(CartesianIndices((1:size(dist, 1), min_dist:max_dist))) do p
-    dist[p, size(dist)[3:4]...]
-    end, size(dist)[3:4]...)
-  while true
-    grid[cur[3], cur[4]] = DIR_STR[cur[1]]
-    ((cur[3] == 1 && cur[4] == 2) || (cur[3] == 2 && cur[4] == 1)) && break
-    prev_pos = CartesianIndex(cur[3], cur[4]) + DIRS[OPPOSITE[cur[1]]]
-    if cur[2] != 1
-      cur = CartesianIndex(cur[1], cur[2] - 1, prev_pos)
-      continue
-    end
-    min_cost = MAX_COST
-    min_alt = CartesianIndex(-1, -1)
-    for dir in eachindex(DIRS)
-      (dir == cur[1] || dir == OPPOSITE[cur[1]]) && continue
-      for i in min_dist:max_dist
-        alt = CartesianIndex(dir, i)
-        cost = dist[CartesianIndex(alt, prev_pos)]
-        if cost < min_cost
-          min_alt = alt
-          min_cost = cost
+    for dir in (mod1(u[1] + 1, 4), mod1(u[1] + 3, 4))
+      dim = iseven(dir) ? 1 : 2
+      t_dir = dir == 2 || dir == 3 ? 1 : -1
+      t_begin = u[dim+1] + t_dir * first(range)
+      t_end = clamp(u[dim+1] + t_dir * last(range), 1, size(data, dim))
+      for t in (t_begin:t_dir:t_end)
+        xy = dim == 1 ? CartesianIndex(t, xy_u[2]) : CartesianIndex(xy_u[1], t)
+        v = CartesianIndex(dir, xy)
+        dist_v = get(queue, v, -1)
+        dist_v == -1 && continue
+        Σ_min, Σ_max = minmax(t, xy_u[dim])
+        alt = dist_u +
+              Σ[dim][Σ_max, xy[3-dim]] - Σ[dim][Σ_min, xy[3-dim]] +
+              (t_dir == -1 ? data[xy] - data[xy_u] : 0)
+        if alt < dist_v
+          queue[v] = dist[v] = alt
+          prev[v] = u
         end
       end
     end
-    cur = CartesianIndex(min_alt, prev_pos)
   end
-  print_grid(grid)
+  dist, prev
 end
 
-data = stack([c - '0' for c in line] for line in eachline())
-for (i, (min_dist, max_dist)) in enumerate(((1, 3), (4, 10)))
-  dist = dijkstra(data, min_dist, max_dist)
-  # print_path(data, dist, min_dist, max_dist)
-  println("Part ", i, " ",
-    minimum(dist[:, min_dist:max_dist, size(data)...]))
+@views function print_path(
+  data::Matrix{Int}, prev::Array{CartesianIndex{3},3}, dest::CartesianIndex{3})
+  grid = data .|> (x -> '0' + x)
+  cur = dest
+  while true
+    p = prev[cur]
+    p[1] == -1 && break
+    if p[2] == cur[2]
+      if p[3] < cur[3]
+        grid[p[2], p[3]:cur[3]] .= 'v'
+      else
+        grid[p[2], cur[3]:p[3]] .= '^'
+      end
+    else
+      if p[2] < cur[2]
+        grid[p[2]:cur[2], p[3]] .= '>'
+      else
+        grid[cur[2]:p[2], p[3]] .= '<'
+      end
+    end
+    cur = p
+  end
+  grid[1, 1] = '0' + data[1, 1]
+  foreach(col -> println(prod(col)), eachcol(grid))
+end
+
+@views data = stack([c - '0' for c in line] for line in eachline())
+for (i, range) in enumerate((1:3, 4:10))
+  dist, prev = dijkstra(data, range)
+  dest = CartesianIndex(argmin(dist[:, size(data)...]), size(data)...)
+  # print_path(data, prev, dest)
+  println("Part ", i, " ", dist[dest])
 end
